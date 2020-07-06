8b2e579cbe4573f0edaf1b85718a8dbbc9accc91
HamBoneGS <59839249+HamBoneGS@users.noreply.github.com>
2020-07-05 19:18:42 -0500

    added click module to allow for more CLI options

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,7 +1,17 @@
 import boto3
+import click
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
     for i in ec2.instances.all():
         print (i)
+
+if __name__ == '__main__':
+
+    list_instances()
+    
